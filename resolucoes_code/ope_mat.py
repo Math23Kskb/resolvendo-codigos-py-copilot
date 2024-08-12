af44c3b375c37d444d933d4d6dc88f53929cee46
Math23Kskb <92028037+Math23Kskb@users.noreply.github.com>
2024-08-12 00:32:42 -0300

    Update ope_mat.py

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,17 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+numero1 = float(input("Digite o primeiro número: "))
+
+
+numero2 = float(input("Digite o segundo número: "))
+
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print("O resultado da soma é:", soma)
+print("O resultado da subtração é:", subtracao)
+print("O resultado da multiplicação é:", multiplicacao)
+print("O resultado da divisão é:", divisao)
