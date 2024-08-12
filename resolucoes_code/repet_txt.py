0d99acdd2877861990edb234d80d9cfebdf2ba7f
Math23Kskb <92028037+Math23Kskb@users.noreply.github.com>
2024-08-12 00:33:10 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,26 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+numero1 = float(input("Digite o primeiro número: "))
+
+numero2 = float(input("Digite o segundo número: "))
+
+operacao = input("Digite a operação desejada (+, -, *, /): ")
+
+# Realiza a operação selecionada pelo usuário
+if operacao == '+':
+    resultado = numero1 + numero2
+elif operacao == '-':
+    resultado = numero1 - numero2
+elif operacao == '*':
+    resultado = numero1 * numero2
+elif operacao == '/':
+    # Verifica se o divisor não é zero para evitar divisão por zero
+    if numero2 != 0:
+        resultado = numero1 / numero2
+    else:
+        resultado = "Erro: Divisão por zero não é permitida."
+else:
+    resultado = "Operação inválida."
+
+# Exibe o resultado da operação
+print("O resultado é:", resultado)
